81fd78078a19e03ffc70a84bee355b72a375838f
saad7836 <60208897+saad7836@users.noreply.github.com>
2020-04-20 04:36:03 +0530

    Add files via upload

---
+++ pairs.java
@@ -0,0 +1,93 @@
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int pairs(int k, int[] arr) {
+
+        Arrays.sort(arr);
+        int count = 0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j] - arr[i] == k || arr[i] - arr[j] == k){
+                    count++;
+                }
+                else if(arr[j] - arr[i] > k) {
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int result = pairs(k, arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+//////////////////////////////////////////////////////
+
+This is another solution . I hv got this logic in discussion section of hacker rank and then implemented it by my own.
+
+public class Solution {
+
+    
+    static int pairs(int k, int[] arr) {
+
+     Arrays.sort(arr);
+     int i=0,j=1,c=0;
+    
+    while(j < arr.length) {
+        
+        
+        if (arr[j] - arr[i] > k) 
+        {
+            i++;
+        } 
+        else if (arr[j] - arr[i] == k) 
+        {
+            c++;
+            j++;
+        } 
+        else if (arr[j] - arr[i] < k)
+         {
+            j++;
+         }
+
+    }
+return c;
+    }
